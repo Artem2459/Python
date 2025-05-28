fa716606c260d8cb0b1ee3ea6a85bf4351c20422
Artem2459 <130496996+Artem2459@users.noreply.github.com>
2025-05-28 15:23:03 +0200

    Add files via upload

--- hw3.py
+++ hw3.py
@@ -1,11 +1,15 @@
-number = int(input("Введіть п'ятизначне число: "))
-
-digit1 = number % 10
-digit2 = (number // 10) % 10
-digit3 = (number // 100) % 10
-digit4 = (number // 1000) % 10
-digit5 = number // 10000
-
-reversed_number = digit1 * 10000 + digit2 * 1000 + digit3 * 100 + digit4 * 10 + digit5
-
-print("Перевернуте число:", reversed_number)
+# # 1.number = int(input("Введіть п'ятизначне число: "))
+#
+# digit1 = number % 10
+# digit2 = (number // 10) % 10
+# digit3 = (number // 100) % 10
+# digit4 = (number // 1000) % 10
+# digit5 = number // 10000
+#
+# reversed_number = digit1 * 10000 + digit2 * 1000 + digit3 * 100 + digit4 * 10 + digit5
+#
+# print("Перевернуте число:", reversed_number)
+# 2.
+# number = input("Введіть п'ятизначне число: ")
+# reversed_number = number[::-1]
+# print("Перевернуте число:", reversed_number)
