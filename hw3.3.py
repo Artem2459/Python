68468a94df3e9f364d3c45fde8b73ba7cc190426
Artem2459 <130496996+Artem2459@users.noreply.github.com>
2025-06-10 18:49:52 +0200

    Add files via upload

---
+++ hw3.3.py
@@ -0,0 +1,11 @@
+def split_list(lst):
+    n = len(lst)
+    middle = (n + 1) // 2
+    return [lst[:middle], lst[middle:]]
+
+# Приклади:
+print(split_list([1, 2, 3, 4, 5, 6]))
+print(split_list([1, 2, 3]))
+print(split_list([1, 2, 3, 4, 5]))
+print(split_list([1]))
+print(split_list([]))
